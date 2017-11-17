8f01163aec350f1f7f1f47c861c9bbe99a14a717
bpk <bkeating@gmail.com>
2017-11-17 11:12:46 -0600

    Designy shit

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 import io from 'socket.io-client';
 
+import 'font-awesome/css/font-awesome.css';
 import './Client.css';
 
 // Variables pulled in from env vars. We share these vars with the backend.
@@ -35,15 +37,33 @@ export default function App() {
   return (
     <div className="App">
 
-      {_.map(PIN_LIST, pin => (
-        <button
-          key={pin}
-          onMouseDown={setPin(pin, 'low')} onTouchStart={setPin(pin, 'low')}
-          onMouseUp={setPin(pin, 'high')} onTouchEnd={setPin(pin, 'high')}
-        >
-          {pin}
-        </button>
-      ))}
+      <div className="header">
+        <FontAwesome name="angle-left" />
+        <img src="./logo.png" className="logo" alt="easttroy.org" />
+        <div className="project-name">
+          Village Square Lights <FontAwesome name="info-circle" fixedWidth />
+        </div>
+      </div>
+
+      <div className="pads">
+        <label><FontAwesome name="map-marker" fixedWidth /> Ben's Office</label>
+        {_.map(PIN_LIST, pin => (
+          <button
+            key={pin}
+            onMouseDown={setPin(pin, 'low')} onTouchStart={setPin(pin, 'low')}
+            onMouseUp={setPin(pin, 'high')} onTouchEnd={setPin(pin, 'high')}
+          >
+            {pin}
+          </button>
+        ))}
+      </div>
+
+      <div className="more">
+        <ul>
+          <li><a href="#">Add More Lights! <FontAwesome name="angle-right" /></a></li>
+          <li><a href="#">Become a Sponsor <FontAwesome name="angle-right" /></a></li>
+        </ul>
+      </div>
 
     </div>
   );
